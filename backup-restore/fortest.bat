cf2cfff4b28d54e53ba91bdfc70bdaeb4724835c
kushan-athukorala <kathukorala@mitrai.com>
2018-02-04 11:50:14 +0530

    add backups

--- backup-restore/fortest.bat
+++ backup-restore/fortest.bat
@@ -3,7 +3,7 @@ FOR /F "tokens=*" %%i IN ('docker images --format "{{.ID}} {{.Repository}}:{{.Ta
     for /F "tokens=1,2" %%a in ("%%i") do (
         echo %%a
         echo %%b
-        docker save -o %%a.tar %%b
+        docker save -o D:\data\docker\image-backups\%%a.tar %%b
     )
 )
     ::for /F "tokens=* delims=:" %%a in (%%i) do (
--- backup-restore/image-backup.bat
+++ backup-restore/image-backup.bat
@@ -1,2 +1,9 @@
 ::FOR /F "tokens=*" %%i IN ('docker images --format "{{.Repository}}-{{.Tag}}"') DO docker save %%i -o D:\data\docker\image-backups\%%i.tar
-FOR /F "tokens=*" %%i IN ('docker images -q') DO docker save %%i -o D:\data\docker\image-backups\%%i.tar
+::FOR /F "tokens=*" %%i IN ('docker images -q') DO docker save %%i -o D:\data\docker\image-backups\%%i.tar
+FOR /F "tokens=*" %%i IN ('docker images --format "{{.ID}} {{.Repository}}:{{.Tag}}"') DO (
+    for /F "tokens=1,2" %%a in ("%%i") do (
+        echo %%a
+        echo %%b
+        docker save -o D:\data\docker\image-backups\%%a.tar %%b
+    )
+)
